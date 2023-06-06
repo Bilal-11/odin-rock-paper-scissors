c97043da0b1a87a48c6ecee32cd68a76c61baaaf
Bilal <88664105+Bilal-11@users.noreply.github.com>
2023-06-06 08:19:22 +0530

    Add solution_plan and Add comments based on solution_plan to script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("it works!");
+// Webpage prompt, user input
+
+// Computer chooses
+
+// A round of rock paper scissors can be played using this function
+
+// 5 rounds are played, scores are kept and finally winner is declared
