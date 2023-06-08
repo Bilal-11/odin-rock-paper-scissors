9a48aa496c34fdbdf7c97eeb9ab76c988b6e4f1d
Bilal <88664105+Bilal-11@users.noreply.github.com>
2023-06-08 06:47:54 +0530

    Add game function to play 5 rounds of the game. Game logic complete.

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-let playerSelection;
-
 /* In a round, 
  winner = 0 => player wins
  winner = 1 => computer wins
@@ -8,8 +6,12 @@ let playerSelection;
 let winner;
 
 // Webpage prompt, user input
-playerSelection = prompt("Choose your weapon (Rock / Paper/ Scissors): ");
-playerSelection = playerSelection.toUpperCase();
+function getPlayerChoice()
+{
+    let playerSelection = prompt("Choose your weapon (Rock / Paper/ Scissors): ");
+    playerSelection = playerSelection.toUpperCase();
+    return playerSelection;
+}
 
 // Computer chooses
 function getComputerChoice()
@@ -74,3 +76,63 @@ function playRound(playerSelection, computerSelection)
 }
 
 // 5 rounds are played, scores are kept and finally winner is declared
+
+function game()
+{
+    /* 
+    Array to maintain the score across rounds
+    scores[0]: Player score
+    scores[1]: Computer score
+    */
+    let scores = [0,0];
+    
+    for(let i = 1; i <= 5; i++)
+    {
+        let playerSelection = getPlayerChoice();
+        let computerSelection = getComputerChoice();
+
+        let resultText = playRound(playerSelection,computerSelection);
+        console.log(resultText);
+
+        switch(winner)
+        {
+            case 0:
+                scores[0]++;
+                break;
+            case 1:
+                scores[1]++;
+                break;
+        }
+
+        let scoreText = `Score:
+        Player:${scores[0]}
+        Computer:${scores[1]}`;
+
+        console.log(`After round ${i},`);
+        console.log(scoreText);
+
+        if(i<5)
+        {
+            console.log("Next round...");
+        }
+    }
+
+    let matchResultText;
+    
+    if(scores[0] > scores[1])
+    {
+        matchResultText = "Player wins the match!";
+    }
+    else if(scores[0] < scores[1])
+    {
+        matchResultText = "Computer wins the match!";
+    }
+    else
+    {
+        matchResultText = "Match is a draw!";
+    }
+
+    console.log(matchResultText);
+}
+
+game();
